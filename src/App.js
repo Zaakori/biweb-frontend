146a7e872b2b7935e7d349a937397cc17df6a9fb
Adel Tiisler <tiisleradel@gmail.com>
2022-10-03 18:32:43 +0300

    now can upload a .txt file to the backend

--- src/App.js
+++ src/App.js
@@ -13,109 +13,107 @@ class App extends Component {
 	// On file select (from the pop up)
 	onFileChange = event => {
 	
-	// Update the state
-	this.setState({ selectedFile: event.target.files[0] });
-	
-	};
+      // Update the state
+      this.setState({ selectedFile: event.target.files[0] });
+      
+      };
 	
 	// On file upload (click the upload button)
 	onFileUpload = () => {
 	
-	// Create an object of formData
-	const formData = new FormData();
-	
-	// Update the formData object
-	formData.append(
-		"myFile",
-		this.state.selectedFile,
-		this.state.selectedFile.name
-	);
-	
-	// Details of the uploaded file
-	console.log(this.state.selectedFile);
+        // Create an object of formData
+        const formData = new FormData();
+        
+        // Update the formData object
+        formData.append('file', this.state.selectedFile);
+        
+        // Details of the uploaded file
+        console.log(this.state.selectedFile);
 
-  let name = String(this.state.selectedFile.name);
-	
-	// Request made to the backend api
-	// Send formData object
-	//axios.post("api/uploadfile", formData);
-  axios.get("http://localhost:8080/api/publish?message=hello from React, here is file name: " + name);
-	};
+        
+        // Request made to the backend api
+        // Send formData object
+        axios.post("http://localhost:8080/api/upload", formData);
+
+        // my little thingy for checking frontend-bakend connection
+        // let name = String(this.state.selectedFile.name);
+        // axios.get("http://localhost:8080/api/publish?message=hello from React, here is file name: " + name);
+        };
 	
 	// File content to be displayed after
 	// file upload is complete
 	fileData = () => {
 
-if (this.state.selectedFile) {
+        if (this.state.selectedFile) {
 
-      let fileType = String(this.state.selectedFile.type);
-      let fileSize = Number(this.state.selectedFile.size);
-      let fileSizeInMB = ((fileSize /1024)/1024).toFixed(3);
+              let fileType = String(this.state.selectedFile.type);
+              let fileSize = Number(this.state.selectedFile.size);
+              let fileSizeInMB = ((fileSize /1024)/1024).toFixed(3);
 
 
-     if(fileType !== "text/plain"){
+            if(fileType !== "text/plain"){
 
-            return (
-              <div>
-                <h2>Please upload a file of type ".txt"!</h2>
-              </div>
-            )
+                    return (
+                      <div>
+                        <h2>Please upload a file of type ".txt"!</h2>
+                      </div>
+                    )
 
-      } else if(fileSizeInMB > 100){
+              } else if(fileSizeInMB > 100){
 
-            return (
-              <div>
-                <h2>Your file size is {fileSizeInMB} MB and that exceeded the sizelimit of 100MB! 
-                  Please upload a file up to 100MB in size.
-                </h2>
-              </div>
-            )
+                    return (
+                      <div>
+                        <h2>Your file size is {fileSizeInMB} MB and that exceeded the sizelimit of 100MB! 
+                          Please upload a file up to 100MB in size.
+                        </h2>
+                      </div>
+                    )
 
-      }else {
+              }else {
 
+                    return (
+                    <div>
+                      <h2>File Details: </h2>
+                      
+                    <p>File Name: {this.state.selectedFile.name}</p>
+                      
+                    <p>
+                      Last Modified:{" "}
+                      {this.state.selectedFile.lastModifiedDate.toDateString()}
+                      </p>
+
+                    </div>
+                    );
+              }
+
+          } else {
             return (
             <div>
-              <h2>File Details: </h2>
-              
-            <p>File Name: {this.state.selectedFile.name}</p>
-              
-            <p>
-              Last Modified:{" "}
-              {this.state.selectedFile.lastModifiedDate.toDateString()}
-              </p>
-
+              <br />
+              <h4>Choose before Pressing the Upload button</h4>
             </div>
             );
-      }
-
-	} else {
-		return (
-		<div>
-			<br />
-			<h4>Choose before Pressing the Upload button</h4>
-		</div>
-		);
-	}
-	};
+          }
+          };
 	
 	render() {
 	
-	return (
-		<div>
-			<h1>
-			Text Processing App
-			</h1>
-			<p>Upload a .txt file up to 100MB in size and see how often each word comes up</p>
-			<div>
-				<input type="file" onChange={this.onFileChange} />
-				<button onClick={this.onFileUpload}>
-				Upload!
-				</button>
-			</div>
-		{this.fileData()}
-		</div>
-	);
-	}
+          return (
+            <div>
+              <h1>
+              Text Processing App
+              </h1>
+              <p>Upload a .txt file up to 100 MB in size and see how often each word comes up</p>
+              <div>
+                <input type="file" onChange={this.onFileChange} />
+                <button onClick={this.onFileUpload}>
+                Upload!
+                </button>
+              </div>
+            {this.fileData()}
+            </div>
+          );
+   }
 }
 
 export default App;
